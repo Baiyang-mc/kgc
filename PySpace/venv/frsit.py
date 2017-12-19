7f247f499b0db647d8a7a3f8b250bf62ac7c970e
baiyang_mc <baiyang_mc@163.com>
2017-12-19 21:53:16 +0800

    NodeJs搭建简单操作命令

---
+++ PySpace/venv/frsit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+print("你好，世界");
+
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 文件名：test.py
+
+if True:
+    print("Answer")
+    print("True")
+else:
+    print ("Answer")
+    # 没有严格缩进，在执行时会报错
+    print ("False")
